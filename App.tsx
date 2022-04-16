ac1bd3726c381ea704e3362437b4eca45973f8ce
Sam Tahiri <sam.tahiri@atomicobject.com>
2022-04-15 20:01:56 -0400

    Update tests

--- App.tsx
+++ App.tsx
@@ -1,21 +1,12 @@
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import {StyleSheet} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {RootNavigator} from './src/navigation/RootNavigator';
+
+import { RootNavigator } from './src/navigation/RootNavigator';
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <RootNavigator />
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <RootNavigator/>
+        </NavigationContainer>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
